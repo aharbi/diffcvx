3dd8268a4a4ce597efc652486844a9ac46dacc65
aalharbi <abdulazizaaharbi@gmail.com>
2024-05-07 19:55:02 -0700

    added economic dispatch model

---
+++ src/model.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+import cvxpy as cp
+import matplotlib.pyplot as plt
+import json
+
+
+class Generator:
+    def __init__(self, specifications: dict):
+
+        self.name = specifications["name"]
+
+        self.alpha = specifications["alpha"]
+        self.beta = specifications["beta"]
+        self.gamma = specifications["gamma"]
+
+        self.max_power = specifications["max_power"]
+        self.min_power = specifications["min_power"]
+
+        self.ramp_rate = specifications["ramp_rate"]
+
+    def compute_cost(self, power: cp.Variable):
+
+        cost = self.alpha * cp.power(power[0], 2) + self.beta * (power) + self.gamma
+
+        return cost.sum()
+
+
+class EconomicDispatchModel:
+    def __init__(self, generators: list[Generator], horizon: int):
+        self.generators = generators
+        self.num_generators = len(generators)
+
+        self.horizon = horizon
+
+        self.model = self.create_ed()
+
+    def create_ed(self):
+        # Parameters
+        self.d = cp.Parameter(self.horizon)
+
+        # Variables
+        self.g = cp.Variable((self.num_generators, self.horizon), nonneg=True)
+
+        # Objetive function
+        objective = 0
+
+        for index, generator in enumerate(self.generators):
+            for t in range(self.horizon):
+                objective += (
+                    generator.alpha * cp.power(self.g[index, t], 2)
+                    + generator.beta * (self.g[index, t])
+                    + generator.gamma
+                )
+
+        # Constraints
+        constraints = []
+
+        # Constraint 1 (Conservation of energy):
+        for t in range(self.horizon):
+            constraints.append(self.g[:, t].sum() == self.d[t])
+
+        # Constraint 2 (Maximum and minimum power constraints):
+        for index, generator in enumerate(self.generators):
+            constraints.append(self.g[index, :] <= generator.max_power)
+            constraints.append(self.g[index, :] >= generator.min_power)
+
+        # Constraint 3 (Maximum and minimum ramp rates):
+        for index, generator in enumerate(self.generators):
+            for t in range(1, self.horizon):
+                constraints.append(
+                    cp.abs(self.g[index, t] - self.g[index, t - 1])
+                    <= generator.ramp_rate
+                )
+
+        model = cp.Problem(cp.Minimize(objective), constraints)
+
+        return model
+
+    def solve_ed(self, demand: np.ndarray):
+
+        self.d.value = demand
+
+        return self.model.solve()
+
+
+if __name__ == "__main__":
+
+    power_system_specs = json.load(open("data/system.json"))
+
+    generators = [Generator(specification) for specification in power_system_specs]
--- src/visualization.py
+++ src/visualization.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+import json
+
+from model import EconomicDispatchModel, Generator
 
 
 plt.rcParams["text.usetex"] = True
 
 
-def plot_caiso_sample(train_dir: str, test_dir: str):
+def visualize_caiso_sample(train_dir: str, test_dir: str):
     """Plots a sample of the hourly electrical load of the CAISO network.
 
     Args:
@@ -21,7 +23,7 @@ def plot_caiso_sample(train_dir: str, test_dir: str):
     train_time_series = train_df["Load"]
     test_time_series = test_df["Load"]
 
-    plt.figure(figsize=(7, 3), dpi=200)
+    plt.figure(figsize=(7, 3), dpi=200, layout="constrained")
 
     plt.plot(
         range(len(train_time_series)),
@@ -38,17 +40,57 @@ def plot_caiso_sample(train_dir: str, test_dir: str):
         label="Testing",
     )
 
-    plt.title("Hourly CAISO Network Load (2022-2023)")
-    plt.ylabel("Electrical Load (MWh)")
+    plt.title("Hourly CAISO Network Load (January 2021 - December 2023)")
     plt.xlabel("Time Index")
+    plt.ylabel("Electrical Load (MWh)")
     plt.legend()
-    plt.tight_layout()
     plt.savefig("figures/sample_caiso_load.png")
+
+
+def visualize_ed_schedule(model: EconomicDispatchModel):
+    """Plots the hourly electrical generation schedule.
+
+    Args:
+        model (EconomicDispatchModel): economic dispatch model object.
+    """
+
+    generation = model.g.value
+    demand = model.d.value
+
+    m, n = generation.shape
+
+    labels = [f"Generator {index + 1}" for index in range(n)]
+
+    plt.figure(figsize=(7, 3), dpi=200, layout="constrained")
+
+    plt.stackplot(range(n), generation, labels=labels)
+    plt.plot(range(n), demand, linestyle="--", c="k", label="Demand")
+
+    plt.title("Hourly Generator Schedule")
+    plt.xlabel("Time (Hour)")
+    plt.ylabel("Electricity Generation (MWh)")
+    plt.legend(ncol=2)
+    plt.savefig("figures/sample_ed_schedule.png")
 
 
 if __name__ == "__main__":
 
+    # Plot a sample of the CAISO data
     train_dir = "data/caiso_train.csv"
     test_dir = "data/caiso_test.csv"
 
-    plot_caiso_sample(train_dir, test_dir)
+    visualize_caiso_sample(train_dir, test_dir)
+
+    # Plot a sample electricity generation schedule
+    power_system_specs = json.load(open("data/system.json"))
+    generators = [Generator(specification) for specification in power_system_specs]
+
+    model = EconomicDispatchModel(generators=generators, horizon=24)
+
+    i = 100
+    df = pd.read_csv("data/caiso_train.csv")
+    load = (df["Load"][i : (i + 24)] * 0.08).to_numpy()
+
+    model.solve_ed(demand=load)
+
+    visualize_ed_schedule(model)
